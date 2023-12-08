5206d1a65e1bcb293029b2fbb5efb00eea132f50
Luis Gustavo Caron <guscar39@gmail.com>
2023-12-08 15:54:37 -0300

    Atualizacao CSS HTML adm

--- db/bd.js
+++ db/bd.js
@@ -2,7 +2,7 @@
 const Client = require('pg').Client
 const db = new Client({
     user: 'postgres',
-    password: "1234",
+    password: "8408",
     host: "localhost",
     port: 5432,
     database: "tcc"
